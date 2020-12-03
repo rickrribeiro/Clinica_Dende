ec1c47fa84d67bb3fb96b9611c0482a0311098b9
rickrribeiro <rickribeiro99@gmail.com>
2020-12-03 19:47:46 -0300

    delete rule by id

--- src/models/appointment-model.js
+++ src/models/appointment-model.js
@@ -21,7 +21,12 @@ writeObj = async (data) => {
         return err
     }
 }
-
+//delete by id
+removeObjbyId = async (rules, id) => {
+    return rules.filter(function(rule) { 
+      return rule.id != id; 
+    });
+  }
 //get all appointments
 getAll = async () => {
     try{
@@ -34,21 +39,33 @@ getAll = async () => {
 //create a new rule
 createRule = async (day, hour) => {
     try{
+        
         var obj = await getObj()
         obj.appointments.push(
             {
                 "id":obj.appointments[obj.appointments.length-1]?obj.appointments[obj.appointments.length-1].id+1:1, //verify if there is any rules and set the id
                 "day":day,
-                "hour":hour
+                "intervals":hour
             })
         writeObj(obj)
         return obj.appointments //return appointments
         
     }catch(err){
         console.log(err)
+        return err
     }
     
 }
 
+//delete rule
+deleteRule = async(id) => {
+    console.log(id)
+    var obj = await getObj()
+    console.log(obj.appointments)
+    console.log("space")
+    obj.appointments = await removeObjbyId(obj.appointments,id);
+    console.log(obj.appointments)
+    await writeObj(obj)
+}
 
-module.exports ={getAll, createRule}
+module.exports ={getAll, createRule, deleteRule}
--- src/resolvers/appointmentResolver.js
+++ src/resolvers/appointmentResolver.js
@@ -1,12 +1,13 @@
 const appointmentModel = require('../models/appointment-model')
-
+const teste =[{"id":1,"day":"weekly","intervals":[{"start":"22","end":"23"}]}]
 
 const appointmentsResolver = {
     Query:{
        appointments: () =>  appointmentModel.getAll() //get all appointments rules
     },
     Mutation: {
-        createAppointment: (_,{day,hour}) => appointmentModel.createRule(day,hour)
+        createAppointment: (_,{day,intervals}) => appointmentModel.createRule(day,intervals),
+        deleteAppointment: (_,{id}) => appointmentModel.deleteRule(id)
     }
 
 }
